0aef342ae1153552a3b62b68876c582e2ca37bb6
anandbadanahatti19 <anandbadanahatti@gmail.com>
2022-11-22 06:20:19 +0530

    removed debugging code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CardComponent } from './card/card.component';
 import { FormComponent } from './form/form.component';
-import { TableApiComponent } from './table-api/table-api.component';
 import { TableComponent } from './table/table.component';
 
 const routes: Routes = [
   { path: 'card', component: CardComponent },
   { path: 'form', component: FormComponent },
   { path: 'table', component: TableComponent },
-  { path: 'table-api', component: TableApiComponent },
 ];
 
 @NgModule({
